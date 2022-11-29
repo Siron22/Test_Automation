707c2b4fcae08a229db3d0821b7b88150d64f60d
Siron22 <s0949528587@gmail.com>
2022-11-29 21:58:08 +0100

    add Homework_10.py

--- Homework/Homework_10.py
+++ Homework/Homework_10.py
@@ -1,19 +1,44 @@
 # Написать декоратор call_times, который будет принимать в качестве параметра file_name,
 # считать количество вызовов функций и записывать в файл в формате f'{func_name} была вызвана {count} раза.\n'
 
-# def decorator(func):
-#         def wrapper(*args, **kwargs):
-#             start = time.time()
-#             result = func(*args, **kwargs)
-#
-#             with open(filename, 'a') as f:
-#                 f.write(f'{func.__name__} {time.time() - start} seconds\n')
-#
-#             return result
-#
-#         return wrapper
-#
-#     return decorator
+
+def call_times(file_name):
+    def inner(func):
+        def wrapper():
+            wrapper.count += 1
+            with open(file_name, 'a') as f:
+                f.write(f'Function {func.__name__} was called {wrapper.count} times.\n')
+            return func()
+
+        wrapper.count = 0
+        return wrapper
+
+    return inner
 
 
+count = 0
 
+
+@call_times('deco_test.txt')
+def baa():
+    pass
+
+
+@call_times('deco_test.txt')
+def daa():
+    pass
+
+
+@call_times('deco_test.txt')
+def zaa():
+    pass
+
+
+for i in range(7):
+    baa()
+
+for i in range(8):
+    daa()
+
+for i in range(5):
+    zaa()
