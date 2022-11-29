27fcd2194a41a3f823d72427aec9563cd4a7ffbc
Siron22 <s0949528587@gmail.com>
2022-11-29 15:04:07 +0100

    Homework_9

--- Homework/Homework_9.py
+++ Homework/Homework_9.py
@@ -3,7 +3,23 @@
 # is_power_of_two(256) # 'yes' потому что 2 в 8 степени это 256
 # is_power_of_two(125) # 'no' потому что это не степень двойки
 
-# Решение 1. Цикл и умножение на два
+# Решение 1. Рекурсия
+
+def is_power_of_two(a: int) -> str:
+    if type(a) == float:
+        print("NO")
+    elif a % 2:
+        return is_power_of_two(a / 2)
+    else:
+        print("YES")
+
+a = int(input("Enter a number: "))
+
+is_power_of_two(a)
+
+
+
+# Решение 2. Цикл и умножение на два
 # def is_power_of_two(a: int) -> str:
 #     i = 1
 #     while i < a:
@@ -18,9 +34,7 @@
 #
 # is_power_of_two(a)
 
-
-
-# Решение 2 и 3. С помощью бинарных операторов. Немного не понял, как работает сохраняю для себя
+# Решение 3 и 4. С помощью бинарных операторов. Немного не понял, как работает сохраняю для себя
 # def is_power_of_two(a: int) -> str:
 #     if a & (a-1):
 #         print("NO")
