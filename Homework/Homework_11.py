d969fbb373deb5e08aac75625315e7b2ed644e5e
Siron22 <s0949528587@gmail.com>
2022-12-10 14:08:10 +0100

    add Homework_11.py

--- Homework/Homework_11.py
+++ Homework/Homework_11.py
@@ -1,31 +1,20 @@
 # Написать функцию, которая возвращает слуайную строку заданной длины.
 # Строка должна состоять из больших и маленьких латинских букв и цифр.
 
-# Решение 1. С помощью random.choice
-# import random
-# import string
-#
-#
-# def get_random_string(length: int) -> str:
-#     character = string.ascii_letters + string.digits
-#     rand_string = ''.join(random.choice(character) for _ in range(length))
-#     return rand_string
-#
-#
-# length = int(input("Введите длину строки: "))
-# print(get_random_string(length))
+# Решение 1. С помощью втроенной функции chr, которая возвращает случайный символ из диапазона ASCII кодов.
+import random
+def get_random_string(length: int) -> str:
+    rand_string = ''
+    for _ in range(length) :
+        low_case = random.randint(97, 122)
+        up_case = random.randint(65, 90)
+        numbers = random.randint(48, 57)
+        character = [low_case, up_case, numbers]
+        a = chr(random.choice(character))
+        rand_string += a
+    return rand_string
 
+length = int(input("Введите длину строки: "))
+print(get_random_string(length))
 
-# Решение 2. Неповторяющиеся значения с помощью random.sample
-# import random
-# import string
-#
-# def get_random_string(length):
-#     character = string.ascii_letters + string.digits
-#     rand_string = ''.join(random.sample(character, length))
-#     return rand_string
-#
-#
-# length = int(input("Введите длину строки: "))
-# print(get_random_string(length))
 
