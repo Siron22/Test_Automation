a8873465f219b44a2065b7d2bdc2cd735f8e185c
Siron22 <s0949528587@gmail.com>
2023-02-27 01:30:00 +0100

    HW_30

--- Homework/HW_30_SQL_class/BD_class2.py
+++ Homework/HW_30_SQL_class/BD_class2.py
@@ -1,9 +1,12 @@
 import sqlite3
 
+
 class UsersTable:
+    CONNECTION = None
 
     def __init__(self, path:str):
         self.connection = sqlite3.connect(path, isolation_level=None)
+        UsersTable.CONNECTION = self.connection
         self.cursor = self.connection.cursor()
         create_users_table = """
         CREATE TABLE IF NOT EXISTS Users (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT NOT NULL,
@@ -11,10 +14,18 @@ class UsersTable:
         """
         self.cursor.execute(create_users_table)
         print('New table created successful')
-
-    def close_connection(self):
         self.connection.close()
 
+    @staticmethod
+    def _open_close_connection(method):
+        def inner(*args, **kwargs):
+            UsersTable.CONNECTION()
+            UsersTable.CONNECTION.cursor()
+            method(*args, **kwargs)
+            UsersTable.CONNECTION.close()
+        return inner
+
+    @_open_close_connection
     def add_one_user(self, name:str, age:int = None, gender:str = None, nationality:str = None):
         add_one_user = "INSERT INTO Users (name, age, gender, nationality) VALUES (?, ?, ?, ?);"
         self.cursor.execute(add_one_user, (name,age,gender,nationality))
@@ -61,17 +72,17 @@ users_data = [('James', 25, 'male', 'USA'),('Leila', 32, 'female', 'France'),('B
 
 users = UsersTable('Test_DB.db')
 users.add_one_user('Rikky', age=60, gender='male', nationality='Uruguay')
-users.get_all_users()
-print('*' * 40)
-users.add_many_users(users_data)
-users.get_all_users()
-print('*' * 40)
-users.get_user_by_gender("female")
-print('*' * 40)
-users.delete_all_olds(50)
-users.get_all_users()
-print('*' * 40)
-users.new_name_for_men("Benya")
-users.get_all_users()
-users.close_connection()
+# users.get_all_users()
+# print('*' * 40)
+# users.add_many_users(users_data)
+# users.get_all_users()
+# print('*' * 40)
+# users.get_user_by_gender("female")
+# print('*' * 40)
+# users.delete_all_olds(50)
+# users.get_all_users()
+# print('*' * 40)
+# users.new_name_for_men("Benya")
+# users.get_all_users()
+# users.connection.close()
 
