39e12dd7184ddfeec85ea4165b3313fc5db140ab
Siron22 <s0949528587@gmail.com>
2023-03-15 14:48:32 +0100

    refactor pages and tests

--- Lecture_code/L21_XPath_Selenium_project./pytest-check.py
+++ Lecture_code/L21_XPath_Selenium_project./pytest-check.py
@@ -1,46 +1,47 @@
-import pytest_check
+from pytest_check import check
+import selenium.webdriver as webdriver
+from selenium.webdriver.common.by import By
+import pytest_check as check
+
+# def test_check():
+#     with pytest_check.check:
+#         #with pytest_check.raises(Exception):
+#             #raise Exception("Exception raised")
+#         assert 1 == 2, "Failed 1==2"
+#     with pytest_check.check:
+#         assert 2 == 2
+#     with pytest_check.check:
+#         assert 2 == 5, "Failed 2==5"
+
+def test_register_user():
+    # Arrange
+    url = "https://docket-test.herokuapp.com/register"
+    # set the driver instance
+    driver = webdriver.Chrome()
+    # browse to the endpoint
+    driver.get(url)
+    # maximise the window
+    driver.maximize_window()
+    # Act
+    # Complete registration form
+    # enter username value
+    driver.find_element(By.ID, "username").send_keys("Ryan8")
+    # enter email value
+    driver.find_element(By.ID, "email").send_keys("Test@email8.com")
+    # enter password value
+    driver.find_element(By.ID, "password").send_keys("12345")
+    # enter repeat password value
+    driver.find_element(By.ID, "password2").send_keys("12345")
+    # click register button
+    driver.find_element(By.ID, "submit").click()
+    # Assert
+    # confirm registration has been successful
+    # check if congratulations message contains the correct text
+    message = driver.find_element(By.XPATH, "/html[1]/body[1]/div[1]/div[1]/div[1]/div[1]/form[1]/div[1]").text
+    check.equal(message, "Congratulations, you are now registered")
+    # check user is routed to login page
+    current_url = driver.current_url
+    check.is_in("login", current_url)
+    driver.quit()
 
 
-def test_check():
-    with pytest_check.check:
-        #with pytest_check.raises(Exception):
-            #raise Exception("Exception raised")
-        assert 1 == 2, "Failed 1==2"
-    with pytest_check.check:
-        assert 2 == 2
-    with pytest_check.check:
-        assert 2 == 5, "Failed 2==5"
-
-
-
-
-# def test_pytest_check():
-#     with pytest_check.check:
-#         assert 2 == 3
-#     check.equal(2, 3)
-#     assert True
-
-# @allure.description('Test for all main paige elements visibility')
-# def test_main_page_elements_displayed(driver):
-#     main_page = MainPage(driver)
-#     main_page.navigate_to_main()
-#     pytest_check.check.is_true(main_page.video_locator.is_displayed())
-#     with pytest_check.check:
-#         assert main_page.logo_hillel_auto.is_displayed()
-#     with pytest_check.check:
-#         assert main_page.button_home.is_displayed()
-#     with pytest_check.check:
-#         assert main_page.button_about.is_displayed()
-#     with pytest_check.check:
-#         assert main_page.button_contacts.is_displayed()
-#     with pytest_check.check:
-#         assert main_page.button_guest_login.is_displayed()
-#     with pytest_check.check:
-#         assert main_page.button_signin.is_displayed()
-#     with pytest_check.check:
-#         assert main_page.button_signup.is_displayed()
-#     with pytest_check.check:
-#         assert main_page.string1.is_displayed()
-#     with pytest_check.check:
-#         assert main_page.string2.is_displayed()
-#      pytest_check.check.is_true(main_page.video_locator.is_displayed())
